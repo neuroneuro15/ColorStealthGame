5f8bf94e4750d5e7de8dd620b5f8ff42f4cb1f25
nickdg <delgrosso@bio.lmu.de>
2017-08-26 18:10:35 +0200

    pretty much just refactored into oop

--- app.py
+++ app.py
@@ -1,94 +1,106 @@
 import pygame
-from pygame import locals
+from pygame.locals import *
 from random import randint
 import itertools
 import cfg
 
 
+class Tile:
 
-def make_player():
-    return {'x': randint(0, cfg.board_size), 'y': randint(0, cfg.board_size)}
+    def __init__(self, x, y, px, py, r, g, b):
+        self.rect = pygame.Rect(x * px, y * py, px, py)
+        self.color = color.Color(r, g, b, a=255)
 
-
-def move_player(board, player, x, y):
-    x_new, y_new = player['x'] + x, player['y'] + y
-    if (0 <= x_new < len(board[0])) and (0 <= y_new < len(board)):
-        player['x'] = x_new
-        player['y'] = y_new
-        new_tile = board[y_new][x_new]
-        color = tuple(randint(0, 255) for _ in range(3))
-        pygame.draw.rect(screen, color, new_tile)
+    def draw(self, screen):
+        r, g, b = self.color.r, self.color.g, self.color.b
+        pygame.draw.rect(screen, (r, g, b), self.rect)
 
 
-def blast_board(board):
-    for tile in itertools.chain(*board):
-        color = tuple(randint(0, 255) for _ in range(3))
-        pygame.draw.rect(screen, color, tile)
+class Player:
+
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    @property
+    def xy(self):
+        return self.x, self.y
 
 
-player_1 = make_player()
-player_2 = make_player()
+class Game:
+
+    def __init__(self, width, height):
+
+        self.screen = pygame.display.set_mode(cfg.screen_resolution)
+        self.screen.fill((100, 0, 0))
+
+        self.clock = pygame.time.Clock()
+
+        self.width = width
+        self.height = height
+        self.board = self.generate_board()
+        self.players = [Player(name='Player 1', x=randint(0, cfg.board_size), y=randint(0, cfg.board_size)),
+                        Player(name='Player 2', x=randint(0, cfg.board_size), y=randint(0, cfg.board_size)),]
+
+    def move_player(self, player, dx, dy, dt):
+        x, y = player.x + dx, player.x + dy
+        if (0 <= x < self.width) and (0 <= y < self.height):
+            player.x, player.y = x, y
+            new_tile = self.board[y][x]
+            new_tile.color = color.Color(randint(0, 255), randint(0, 255), randint(0, 255), a=255)
+            self.check_for_win(player)
+
+    def check_for_win(self, player_moving):
+        for p1, p2 in itertools.combinations(self.players, 2):
+            if p1.xy == p2.xy:
+                print('Contact! {} Wins!'.format(player_moving.name))
+
+    def draw(self):
+        for tile in itertools.chain(*self.board):
+            tile.draw(self.screen)
+        pygame.display.flip()
+
+    def generate_board(self):
+        px = self.screen.get_width() // self.width
+        py = self.screen.get_height() // self.height
+        return [[Tile(x, y, px, py, randint(0, 255), randint(0, 255), randint(0, 255),) for x in range(self.width)] for y in range(self.height)]
+
+    def handle_keys(self, dt, event):
+        movement_inputs = {
+            K_UP: (self.players[0], 0, -1),
+            K_DOWN: (self.players[0], 0, 1),
+            K_LEFT: (self.players[0], -1, 0),
+            K_RIGHT: (self.players[0], 1, 0),
+            K_w: (self.players[1], 0, -1),
+            K_s: (self.players[1], 0, 1),
+            K_a: (self.players[1], -1, 0),
+            K_d: (self.players[1], 1, 0),
+        }
+
+        if event.key == K_ESCAPE:
+            pygame.quit()
+        else:
+            pressed = pygame.key.get_pressed()
+            for key, (player, x, y) in movement_inputs.items():
+                if pressed[key]:
+                    self.move_player(player, x, y, dt)
 
 
-def check_for_win(players, player_moving):
-    for p1, p2 in itertools.combinations(players, 2):
-        if p1['x'] == p2['x'] and p1['y'] == p2['y']:
-            name = 'Player 1' if player_moving == player_1 else 'Player 2'
-            print('Contact! {} Wins!'.format(name))
+    def run(self):
+        while True:
+            dt = self.clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    self.handle_keys(dt, event)
 
-
-
-
-def handle_keys(dt):
-    movement_inputs = {
-        locals.K_UP: (player_2, 0, -1),
-        locals.K_DOWN: (player_2, 0, 1),
-        locals.K_LEFT: (player_2, -1, 0),
-        locals.K_RIGHT: (player_2, 1, 0),
-        locals.K_w: (player_1, 0, -1),
-        locals.K_s: (player_1, 0, 1),
-        locals.K_a: (player_1, -1, 0),
-        locals.K_d: (player_1, 1, 0),
-    }
-
-
-
-    if event.key == locals.K_ESCAPE:
-        pygame.quit()
-    else:
-        pressed = pygame.key.get_pressed()
-        for key, (player, x, y) in movement_inputs.items():
-            if pressed[key]:
-                move_player(board, player, x, y)
-                check_for_win(players=[player_1, player_2],
-                              player_moving=player)
-
-
-def update_game(dt):
-    for ai in ai_players:
-        move_player(board, ai, randint(-1, 1), randint(-1, 1))
+            self.draw()
 
 
 
 if __name__ == '__main__':
 
-    screen = pygame.display.set_mode(cfg.screen_resolution)
-    screen.fill((0, 0, 0))
-    px = screen.get_width() // cfg.board_size
-    py = screen.get_height() // cfg.board_size
+    game = Game(width=cfg.board_size, height=cfg.board_size)
+    game.run()
 
-    board = [[pygame.Rect(x * px, y * py, px, py) for x in range(cfg.board_size)] for y in range(cfg.board_size)]
-
-    ai_players = [make_player() for _ in range(cfg.ai_players)]
-
-    blast_board(board)
-    clock = pygame.time.Clock()
-    while True:
-        dt = clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == locals.KEYDOWN:
-                handle_keys(dt)
-
-        update_game(dt)
-        pygame.display.flip()
 
