9d50cc6b18984be43301b55fecf03466c0b219de
nickdg <delgrosso@bio.lmu.de>
2017-08-27 14:27:42 +0200

    started setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='ColorStealthGame',
+    version='0.1',
+    packages=['colorgame'],
+    install_requires=[
+        'pygame'
+    ],
+    scripts = ['']
+    entry_points={
+        'console_scripts': ['colorgame=colorgame.app:main'],
+    },
+    url='https://www.github.com/neuroneuro15/ColorStealthGame',
+    license='MIT',
+    author='Nicholas A. Del Grosso and Anna Durbanova',
+    author_email='delgrosso.nick@gmail.com',
+    description='Abstract Pygame made for mini game jam around the theme "invisibilitity".  Two Players have to find themselves in a chaotic sea, then find each other!'
+)
