e220f0d7350996439839ff1f9f67a817cf2106ad
nickdg <delgrosso@bio.lmu.de>
2017-08-27 14:37:39 +0200

    Added working entry point, for launching the game from anywhere.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='ColorStealthGame',
@@ -7,7 +7,6 @@ setup(
     install_requires=[
         'pygame'
     ],
-    scripts = ['']
     entry_points={
         'console_scripts': ['colorgame=colorgame.app:main'],
     },
